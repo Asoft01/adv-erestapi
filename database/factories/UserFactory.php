a9879d342223a90221e6d0003ee395b8a733be27
Adeleke Hammed <lekhad19@gmail.com>
2021-09-30 17:23:53 +0100

    Implementing The Show Method for SellerController

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,6 +1,12 @@
 <?php
 
-use Faker\Generator as Faker;
+// use Faker\Generator as Faker;
+
+use App\Category;
+use App\Product;
+use App\Seller;
+use App\User;
+use App\Transaction;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,11 +19,50 @@ use Faker\Generator as Faker;
 |
 */
 
-$factory->define(App\User::class, function (Faker $faker) {
+$factory->define(User::class, function (Faker\Generator $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
         'remember_token' => str_random(10),
+        'verified' => $verified  = $faker->randomElement([User::VERIFIED_USER, User::UNVERIFIED_USER]),
+        'verification_token' =>  $verified == User::VERIFIED_USER ? null : User::generateVerificationCode(),
+        'admin' => $verified = $faker->randomElement([User::ADMIN_USER, User::REGULAR_USER])
+    ];
+});
+
+
+$factory->define(Category::class, function (Faker\Generator $faker) {
+    return [
+        'name' => $faker->word,
+        'description' => $faker->paragraph(1),
+    ];
+});
+
+
+$factory->define(Product::class, function (Faker\Generator $faker) {
+    return [
+        'name' => $faker->word,
+        'description' => $faker->paragraph(1),
+        'quantity' => $faker->numberBetween(1, 10),
+        'status' => $faker->randomElement([Product::AVAILABLE_PRODUCT, Product::UNAVAILABLE_PRODUCT]),
+        'image' => $faker->randomElement(['1.jpg', '2.jpg', '3.jpg']),
+        'seller_id' => User::all()->random()->id,
+        // User::inRandomOrder()->first()->id
+    ];
+});
+
+
+
+$factory->define(Transaction::class, function (Faker\Generator $faker) {
+    // Seller has at least one product
+    $seller = Seller::has('products')->get()->random();
+    // Buyer must be different from the seller, regular user before buying
+    $buyer = User::all()->except($seller->id)->random();
+
+    return [
+        'quantity' => $faker->numberBetween(1, 3),
+        'buyer_id' => $buyer->id,
+        'product_id' => $seller->products->random()->id 
     ];
 });
