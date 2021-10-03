8c7ed494014a79c99f8fe8eee31732507b0f86f7
Adeleke Hammed <lekhad19@gmail.com>
2021-10-03 23:02:44 +0100

    Fetching the transactions from category

---
+++ app/Http/Controllers/Category/CategoryTransactionController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\Category;
+
+use App\Category;
+use App\Http\Controllers\ApiController;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class CategoryTransactionController extends ApiController
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Category $category)
+    {
+        // The where has is used to pull out on the the products that has at least one transaction because there is possibility for some of them 
+        // not to have transaction
+        $transactions = $category->products()
+                        ->whereHas('transactions')
+                        ->with('transactions')
+                        ->get()
+                        ->pluck('transactions')
+                        ->collapse();
+        return $this->showAll($transactions);
+
+
+    }
+}
